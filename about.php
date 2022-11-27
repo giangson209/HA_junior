7ad50b01a39ce098b00637459512895a097200a7
giangson209 <ledinhson209@gmail.com>
2022-11-28 03:50:55 +0700

    update f

---
+++ about.php
@@ -0,0 +1,95 @@
+<?php include 'header.php';?>
+<main>
+	<section class="box-breadcrumbs">
+		<div class="container">
+			<div class="cap-bread text-center">
+				<h2>Giới thiệu</h2>
+				<ul>
+					<li><a href="">Trang chủ</a></li>
+					<li><a href="">Giới thiệu</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<section class="content-about">
+		<div class="container">
+			<div class="tab-about">
+				<ul>
+					<li><a href="javascript:void(0)" data-tab="tab-1" class="active">Về HA Centrer Junior</a></li>
+					<li><a href="javascript:void(0)" data-tab="tab-2">Tầm nhìn - Sứ mệnh - Cam kết </a></li>
+					<li><a href="javascript:void(0)" data-tab="tab-3">Đội ngũ giáo viên</a></li>
+					<li><a href="javascript:void(0)" data-tab="tab-4">Thành tựu</a></li> 
+					<li><a href="javascript:void(0)" data-tab="tab-5">Cơ sở vật chất</a></li>
+				</ul>
+			</div>
+			<div class="content-detail-about">
+				<div class="content-about active" id="tab-1">
+					<div class="detail-about-top">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="detail">
+									<p>Trung tâm ngoại ngữ HA Centre Junior là một tổ chức giáo dục hàng đầu trong lĩnh vực đào tạo Tiếng Anh. Trực thuộc Công ty TNHH Phát Triển Giáo Dục HA do Mr. Nguyen Manh Ha sáng lập vào năm 2015, với mong muốn trở thành một thương hiệu vững chắc cung cấp các chương trình học tiếng Anh chất lượng cao cho trẻ em, góp phần xóa nhòa khoảng cách về nền tảng giáo dục tiếng Anh giữa các tỉnh thành và thủ đô Hà Nội.</p>
+									<p>Trong vòng 7 năm hoạt động, HA Centre Junior đã có mặt tại Hà Nội và Bắc Ninh. Với giá trị cốt lõi được đặt ra là lấy học viên làm trung tâm, tất cả mọi hoạt động của Trung tâm ngoại ngữ HA Centre Junior đều lấy chất lượng và sự tận tâm làm kim chỉ nam để thực hiện!</p>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="avarta"><img src="images/video-about.png" class="img-fluid" alt=""></div>
+							</div>
+						</div>
+					</div>
+					<div class="about-system">
+						<div class="item-system">
+							<div class="t-system">Hệ thống Giáo dục HA Centre Junior</div>
+							<ul>
+								<li>Fanpage: </li>
+								<li>Website: </li>
+								<li>MR. HA: 0963072486</li>
+								<li>Office: Tầng 2, Tòa nhà HTT1, 197 Trần Phú, Hà Đông, Hà Nội</li>
+							</ul>
+						</div>
+						<div class="item-system">
+							<h3><img src="images/place.png" class="img-fluid" alt=""><span>Hà Nội</span></h3>
+							<ul>
+								<li>CS1: Tầng 2, Tòa nhà HTT1, 197 Trần Phú, Hà Đông, Hà Nội</li>
+								<li>CS3: 51 Quan Nhân, Hà Nội</li>
+								<li>CS7: TT. Chúc Sơn, Chương Mỹ, Hà Nội</li>
+							</ul>
+						</div>
+						<div class="item-system">
+							<h3><img src="images/place.png" class="img-fluid" alt=""><span>Bác Ninh</span></h3>
+							<ul>
+								<li>CS2: Khu phố Mới, Thọ Môn, Đình Bảng, Từ Sơn, Bắc Ninh (sau SVĐ Đình Bảng, cách trường THPT Lý Thái Tổ 300m)</li>
+								<li>CS5: 85 Nguyễn Đăng Đạo, TT. Lim, Tiên Du, Bắc Ninh (cách trường THPT Nguyễn Đăng Đạo 600m)</li>
+								<li>CS4: 939 Đường  u Cơ, Thị trấn Hồ, Thuận Thành, Bắc Ninh</li>
+								<li>CS6: 42 Nguyễn Cao, P. Ninh Xá, TP. Bắc Ninh</li>
+								<li>CS8: 326 Phố Mới, Thị Trấn Chờ, Yên Phong, Bắc Ninh</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="content-about" id="tab-2">
+					<div class="detail">
+						<div class="t-system">Tầm nhìn - Sứ mệnh</div>
+						<h3>Tầm nhìn</h3>
+						<p>HA Centre Junior phấn đấu trở thành một tổ chức đào tạo tiếng Anh cho trẻ em có T M và có TẦM trong địa bàn Bắc Ninh và Hà Nội với chất lượng đào tạo tốt, uy tín, tạo sự tin tưởng cao nơi phụ huynh và học sinh, từ đó giúp các em có nguồn kiến thức chắc chắn và tự tin để tạo nền tảng cho một tương lai tươi sáng.</p>
+						<h3>Sứ mệnh</h3>
+						<p>Trang bị kiến thức và kỹ năng nền tảng giúp học viên tự tin đạt kết quả cao trong các kỳ thi tiếng Anh và ứng dụng tiếng Anh trong cuộc sống, công việc để trở thành công dân toàn cầu.</p>
+						<p>HA Centre Junior hoạt động với tôn chỉ hết mình vì học sinh, đồng hành cùng các em trên chặng đường dài tiến bộ, từ thiết lập một nền tảng ngôn ngữ vững chắc cho đến việc tự tin sử dụng ngôn ngữ, thông qua ngôn ngữ để thể hiện bản thân mình với toàn thế giới.</p>
+
+						<div class="t-system">Cam kết</div>
+						<h3>Cam kết chất lượng</h3>
+						<p>HA Centre Junior luôn coi sự hài lòng của khách hàng là một trong những mục tiêu phấn đấu hàng đầu. Chúng tôi có mặt ở đây để giúp các học viên phát huy tối đa năng lực tiếng Anh của mình thông qua những khóa học được đúc kết, nghiên cứu chuyên sâu về phát triển ngôn ngữ, kỹ năng xử lý trong mọi tình huống, cách tư duy lẫn chiến thuật làm bài và sự hướng dẫn tận tình trong một môi trường học tập hiệu quả với đầy đủ cơ sở vật chất trang thiết bị hiện đại.</p>
+						<h3>Cam kết phát triển</h3>
+						<p>Để giữ vững tính chuyên nghiệp, HA Centre Junior luôn đẩy mạnh tăng cường công tác đào tạo đội ngũ giáo viên và nhân viên để luôn xứng đáng là đơn vị đi đầu trong lĩnh vực đào tạo tiếng Anh. HA Centre Junior đề cao năng lực của cả khách hàng và nhân viên. Chúng tôi luôn tìm cách tạo ra một môi trường làm việc cởi mở, hợp tác và năng động, là nơi tất cả mọi người đều có cơ hội được phát triển.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+<?php include 'footer.php';?>
+
+
+
+
+
---
+++ schedule.php
@@ -0,0 +1,143 @@
+<?php include 'header.php';?>
+<main>
+	<section class="box-breadcrumbs">
+		<div class="container">
+			<div class="cap-bread text-center">
+				<h2>Lịch Khai giảng</h2>
+				<ul>
+					<li><a href="">Trang chủ</a></li>
+					<li><a href="">Lịch Khai giảng</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<section class="box-schedule">
+		<div class="container">
+			<div class="shedule-hot">
+				<div class="row align-items-center">
+					<div class="col-md-6">
+						<div class="avarta"><a href=""><img src="images/lich-1.png" class="img-fluid" alt=""></a></div>
+					</div>
+					<div class="col-md-6">
+						<div class="info">
+							<div class="time">Cập nhật 3 giờ trước</div>
+							<h2><a href="">Khai giảng khóa Kiddie A tháng 11 cơ sở Hà Nội</a></h2>
+							<div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id enim, a, risus molestie arcu dignissim aenean. Morbi sollicitudin at eleifend quis a enim scelerisque.</div>
+							<div class="view-detail-big"><a href="">Chi tiết<img src="images/arrow-right-dark.svg" class="img-fluid" alt=""></a></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="list-schedule">
+				<div class="item-date-schedule">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="date-schedule">
+								<div class="time">Tháng 11, 2022</div>
+								<div class="update-time">Cập nhật 3 giờ trước</div>
+							</div>
+						</div>
+						<div class="col-md-9">
+							<div class="item-schedule">
+								<div class="info">
+									<h3><a href="">Khai giảng khóa Kiddie A tháng 11 cơ sở Hà Nội</a></h3>
+									<div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id enim, a, risus molestie arcu dignissim aenean. Morbi sollicitudin at eleifend quis a enim scelerisque. Arcu amet nisi et consequat commodo nulla. Sem fames blandit suspendisse at orci dolor nam.</div>
+									<div class="view-detail"><a href="">Chi tiết<img src="images/arrow-right-dark.svg" class="img-fluid" alt=""></a></div>
+								</div>
+								<div class="avarta"><a href=""><img src="images/lich-2.png" class="img-fluid w-100" alt=""></a></div>
+							</div>
+							<div class="item-schedule">
+								<div class="info">
+									<h3><a href="">Khai giảng khóa Kiddie A tháng 11 cơ sở Hà Nội</a></h3>
+									<div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id enim, a, risus molestie arcu dignissim aenean. Morbi sollicitudin at eleifend quis a enim scelerisque. Arcu amet nisi et consequat commodo nulla. Sem fames blandit suspendisse at orci dolor nam.</div>
+									<div class="view-detail"><a href="">Chi tiết<img src="images/arrow-right-dark.svg" class="img-fluid" alt=""></a></div>
+								</div>
+								<div class="avarta"><a href=""><img src="images/lich-3.png" class="img-fluid w-100" alt=""></a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item-date-schedule">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="date-schedule">
+								<div class="time">Tháng 12, 2022</div>
+								<div class="update-time">Cập nhật 3 giờ trước</div>
+							</div>
+						</div>
+						<div class="col-md-9">
+							<div class="item-schedule">
+								<div class="info">
+									<h3><a href="">Khai giảng khóa Kiddie A tháng 11 cơ sở Hà Nội</a></h3>
+									<div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id enim, a, risus molestie arcu dignissim aenean. Morbi sollicitudin at eleifend quis a enim scelerisque. Arcu amet nisi et consequat commodo nulla. Sem fames blandit suspendisse at orci dolor nam.</div>
+									<div class="view-detail"><a href="">Chi tiết<img src="images/arrow-right-dark.svg" class="img-fluid" alt=""></a></div>
+								</div>
+								<div class="avarta"><a href=""><img src="images/lich-2.png" class="img-fluid w-100" alt=""></a></div>
+							</div>
+							<div class="item-schedule">
+								<div class="info">
+									<h3><a href="">Khai giảng khóa Kiddie A tháng 11 cơ sở Hà Nội</a></h3>
+									<div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id enim, a, risus molestie arcu dignissim aenean. Morbi sollicitudin at eleifend quis a enim scelerisque. Arcu amet nisi et consequat commodo nulla. Sem fames blandit suspendisse at orci dolor nam.</div>
+									<div class="view-detail"><a href="">Chi tiết<img src="images/arrow-right-dark.svg" class="img-fluid" alt=""></a></div>
+								</div>
+								<div class="avarta"><a href=""><img src="images/lich-3.png" class="img-fluid w-100" alt=""></a></div>
+							</div>
+							<div class="item-schedule">
+								<div class="info">
+									<h3><a href="">Khai giảng khóa Kiddie A tháng 11 cơ sở Hà Nội</a></h3>
+									<div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id enim, a, risus molestie arcu dignissim aenean. Morbi sollicitudin at eleifend quis a enim scelerisque. Arcu amet nisi et consequat commodo nulla. Sem fames blandit suspendisse at orci dolor nam.</div>
+									<div class="view-detail"><a href="">Chi tiết<img src="images/arrow-right-dark.svg" class="img-fluid" alt=""></a></div>
+								</div>
+								<div class="avarta"><a href=""><img src="images/lich-3.png" class="img-fluid w-100" alt=""></a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item-date-schedule">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="date-schedule">
+								<div class="time">Tháng 1, 2023</div>
+								<div class="update-time">Cập nhật 3 giờ trước</div>
+							</div>
+						</div>
+						<div class="col-md-9">
+							<div class="item-schedule">
+								<div class="info">
+									<h3><a href="">Khai giảng khóa Kiddie A tháng 11 cơ sở Hà Nội</a></h3>
+									<div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id enim, a, risus molestie arcu dignissim aenean. Morbi sollicitudin at eleifend quis a enim scelerisque. Arcu amet nisi et consequat commodo nulla. Sem fames blandit suspendisse at orci dolor nam.</div>
+									<div class="view-detail"><a href="">Chi tiết<img src="images/arrow-right-dark.svg" class="img-fluid" alt=""></a></div>
+								</div>
+								<div class="avarta"><a href=""><img src="images/lich-2.png" class="img-fluid w-100" alt=""></a></div>
+							</div>
+							<div class="item-schedule">
+								<div class="info">
+									<h3><a href="">Khai giảng khóa Kiddie A tháng 11 cơ sở Hà Nội</a></h3>
+									<div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id enim, a, risus molestie arcu dignissim aenean. Morbi sollicitudin at eleifend quis a enim scelerisque. Arcu amet nisi et consequat commodo nulla. Sem fames blandit suspendisse at orci dolor nam.</div>
+									<div class="view-detail"><a href="">Chi tiết<img src="images/arrow-right-dark.svg" class="img-fluid" alt=""></a></div>
+								</div>
+								<div class="avarta"><a href=""><img src="images/lich-3.png" class="img-fluid w-100" alt=""></a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="pagination">
+				<ul>
+					<li><a href="" class="active">1</a></li>
+					<li><a href="">2</a></li>
+					<li><a href="">3</a></li>
+					<li><a href="">4</a></li>
+					<li><a href="">5</a></li>
+				</ul>
+			</div> 
+		</div>
+	</section>
+</main>
+<?php include 'footer.php';?>
+
+
+
+
+
